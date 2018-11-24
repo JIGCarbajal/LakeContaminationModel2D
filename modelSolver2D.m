c1693149394330b5caaa0c5e12bc3ebd8cf3d573
JIGCarbajal <34818410+JIGCarbajal@users.noreply.github.com>
2018-11-24 11:19:02 -0600

    Update modelSolver2D.m

--- modelSolver2D.m
+++ modelSolver2D.m
@@ -13,16 +13,16 @@
 %
 %	Funciones externas:
 %
-%			     massAssembler2D                                                                            
-%			     stiffnessAssembler2D
-%			    	loadAssembler2D (Dirac version)
-%           		advectionAssembler2D
+%			massAssembler2D                                                                            
+%			stiffnessAssembler2D
+%			loadAssembler2D (Dirac version)
+%			advectionAssembler2D
 %
 %	Datos entrada:
-%			      E	- Escalar, Coeficiente de difusividad
+%			      E		- Escalar, Coeficiente de difusividad
 %			      u 	- Matriz, matriz de velocidades del fluido en cada nodo de la malla en cada instante
 %			      k 	- Escalar, razon temporal de "desintegracion" del contaminante 
-%			      p	- Matriz (2,np), posiciones de los nodos en la malla
+%			      p		- Matriz (2,np), posiciones de los nodos en la malla
 %			      t 	- Matriz (3,nt), conectividad de los nodos en los triangulos de la malla
 %			      f 	- Matlab anonimous function, forzamiento externo del modelo (contaminante)
 %			      nod	- Vector, indices donde se vierte contaminante nod(i)\in [1,np]
@@ -42,49 +42,43 @@ function [uh] = modelSolver2D()
 %===============================================================================
 % Incializacion de parametros del modelo
 %===============================================================================
-E = 5e4;	k = 1e-6;  % Parametros dinamicos del modelo
+	E = 5e4; % Coeficiente de difusividad
+	k = 1e-6;  % Coeficiente de extincion de contaminante
 
-dt = 0.6;
+	dt = 0.6; % paso de tiempo
 
-load 'Cajimalla.mat'
+	load 'Cajimalla.mat' % se cargan los parámetros de malla y las velocidades
   
-p = z_mts;
-t = mm;
-u = zeros(2, size(Vqx,1), size(Vqx,2));
-for i=1:size(Vqx,2)
-	u(1,:,i) = Vqx(:,i);
-	u(2,:,i) = Vqy(:,i);
-	h(:,i) = Vqz(:,i)+h_mts(:); 
-end
+	p = z_mts; % matriz de coordenadas de los puntos de la malla
+	t = mm; % matriz de conectividad de los nodos de la malla
+	u = zeros(2, size(Vqx,1), size(Vqx,2)); % se crea un vector unico de velocidades
+	for i=1:size(Vqx,2)
+		u(1,:,i) = Vqx(:,i); % velocidad en "x" en cada nodo
+		u(2,:,i) = Vqy(:,i); % velocidad en "y" en cada nodo
+		h(:,i) = Vqz(:,i)+h_mts(:);  % profundidad del lago en cada nodo
+	end
  
-
-f = @(t) 0 ;             % Funcion de forzamiento del contaminante
-nod = [100;200;300];              % Punto donde se añade el contaminante
+	f = @(t) 0 ;             % Funcion de forzamiento del contaminante
+	nod = [100;200;300];              % Puntos donde se añade el contaminante
 
 %===============================================================================
 % Calculo de las matrices necesarias para la solucion
 %===============================================================================
-M = massAssembler2D(p,t);
-A = stiffnessAssembler2D(p,t);
+	M = massAssembler2D(p,t); % matriz de masa del sistema
+	A = stiffnessAssembler2D(p,t); % matriz de rigidez del sistema
 
 %===============================================================================
 % Ciclo de calculo temporal para euler hacia atras
 %===============================================================================
-uh(:,1) = zeros(length(p),1);
+uh(:,1) = zeros(length(p),1); % condicion inicial de ceros
 uh(:,1) = exp(-((p(1,:)-p(1,1080)).^2 + (p(2,:)-p(2,1080)).^2 )./(2*100^2)); 
 
-D = advectionAssembler2D(p,t,u,1); % Es constante...
-R = inv(M + dt*(E*A+k*M+D));
+D = advectionAssembler2D(p,t,u,1); % Matriz de adveccion del sistema en cada instante (es constante aquí)
+R = inv(M + dt*(E*A+k*M+D)); % Calculo de la matriz invesa para solucion del sistema
 for i=2:size(Vqx,2)
-	b = loadAssembler2D(p,h(:,i),nod,f(dt*i));	
+	b = loadAssembler2D(p,h(:,i),nod,f(dt*i)); % calculo del vector de carga en cada instante de tiempo
 	uh(:,i) = R*(M*uh(:,i-1)+dt*b);
 end
 
-%figure(2)
-%[X,T] = meshgrid(x,t);
-%surfc(X',T',uh,'EdgeColor','interp')
-%xlabel('x_k');ylabel('t^n');zlabel('u^n_{h,k}')
-
 
 end
-
