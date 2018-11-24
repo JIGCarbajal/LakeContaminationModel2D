b2e4b80e3413792ddb13fab7fd9d35382b27cd55
JIGCarbajal <34818410+JIGCarbajal@users.noreply.github.com>
2018-11-23 23:04:47 -0600

    Update binSrch.m

--- binSrch.m
+++ binSrch.m
@@ -3,24 +3,27 @@
 %		  ordenado "p", con cierto valor de tolerancia "tol" en el valor de "elem"
 %                
 %   	Datos entrada:                                                                                                                               
-%		p   - Matriz (2,np), coordenadas de los puntos de la malla
-%           	t   - Matriz (3,nt), matriz de conectividad de los puntos de la malla
-%           	u   - Matriz (2,np), matriz de las velocidades en "x" y "y" en cada nodo 
-%           	ti  - Escalar, instante de tiempo en el que se calculará la matriz de advección (indice de u(t))
+%           	elem	- Escalar, elemento a buscar en el arreglo 
+%           	tol	- Escalar, tolerancia en el valor de "elem"
+%		p   	- Vector, Arreglo donde se buscara el dato
 %
 %	Funciones externas:
-%			gradPhi()
 %
 %   	Datos salida:
-%           	D   - Matriz (np,np), matriz de adveccion del sistema en el instante "ti"
+%           	x	- Vector, Arreglo con los indices donde se encuentran los elementos entre [elem-tol,elem+tol] 
 % Fecha elaboracion: 3/Nov/2018
 % Ultima actualizacion: 16/Nov/2018
 %--------------------------------------------------------------------------------------------------------------------- 
 
 function [x] = binSrch(elem, tol, p)
 
-        n = length(p);
-        low = 1;
+        n = length(p); % numero de elementos del arreglo
+
+%---------------------------------------------------------------------------------------------------------------------
+% Busqueda binaria para encontrar algun elemento dentro del intervalo [elem-tol,elem+tol]
+%---------------------------------------------------------------------------------------------------------------------
+
+	low = 1;
         high = n;
 	mid=[];
 
@@ -37,26 +40,30 @@ function [x] = binSrch(elem, tol, p)
                 end
         end
 
+%---------------------------------------------------------------------------------------------------------------------
+% Busqueda lineal de elementos hacia adelante y hacia atras de p(mid) dentro del intervalo [elem-tol,elem+tol]
+%---------------------------------------------------------------------------------------------------------------------
+
         j = 0;
         k = 0;
 
-	if(tol)
-        	while(mid-j > 1 && p(mid-j)>elem-tol)
+	if(tol) % si la tolerancia es distinta de "0"
+        	while(mid-j > 1 && p(mid-j)>elem-tol) % busqueda hacia la izquierda
                 	j = j + 1;
         	end
 
-       		while(mid+k < n && p(mid+k)<elem+tol)
+       		while(mid+k < n && p(mid+k)<elem+tol) % busqueda hacia la derecha
                 	k = k + 1;
         	end
 		
 		x=[mid-j:mid+k];
 
-        else
-		while(mid-j > 1 && p(mid-j)==p(mid))
+        else % si la tolerancia es igual a cero, se buscan los elementos iguales
+		while(mid-j > 1 && p(mid-j)==p(mid)) % busqueda hacia la izquierda
 			j = j + 1;
 		end
 
-		while(mid+k < n && p(mid+k)==p(mid))
+		while(mid+k < n && p(mid+k)==p(mid)) % busqueda hacia la derecha
 			k = k + 1;
 		end
 
