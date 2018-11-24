c62cc10110d7e8333c0967709daf347e7986f7ad
JIGCarbajal <34818410+JIGCarbajal@users.noreply.github.com>
2018-11-24 11:09:25 -0600

    Update loadAssembler2D.m

--- loadAssembler2D.m
+++ loadAssembler2D.m
@@ -4,7 +4,7 @@
 %   Datos entrada:                                                                                                                               
 %			p   - Matriz (2,np), coordenadas de los puntos de la malla
 %			h   - Vector (np,1), sobre-elevacion del fluido en los puntos de la malla, en el tiempo de interes
-%           	nod - Vector (npi,1), Indices de la malla donde se añadira la carga
+%     nod - Vector (npi,1), Indices de la malla donde se añadira la carga
 %			f   - Vector, Valor de la funcion que seguira la carga en el instante de tiempo, en cada punto de
 %				 inyeccion 
 %           	ti  - Escalar, Valor del tiempo en el cual se desea calcular el vector de carga
